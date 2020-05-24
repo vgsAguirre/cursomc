ffa185338d79782a1995a26190b17a315ef87bdd
vgsAguirre <vgs.aguirre@gmail.com>
2020-05-24 00:34:52 -0300

    Projeto criado

---
+++ src/test/java/com/nelioalves/cursoms/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelioalves.cursoms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
