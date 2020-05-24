eda920c018744610142766452377084a8f0a9538
vgsAguirre <vgs.aguirre@gmail.com>
2020-05-24 08:42:43 -0300

    REST testado.

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,8 +1,10 @@
-package com.nelioalves.cursoms;
+package com.nelioalves.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class CursomcApplication {
 
